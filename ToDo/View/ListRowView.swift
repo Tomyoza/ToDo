8184d5d52f268151333511a2fb251b1750c66142
Tomyoza <tomoki.yoza@gmail.com>
2025-06-19 02:33:15 -1000

    Created Basic Todo UI

---
+++ ToDo/View/ListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ListRowView.swift
+//  ToDo
+//
+//  Created by Tomoki Yoza on 2025/06/19.
+//
+
+import SwiftUI
+
+struct ListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRowView()
+}
---
+++ ToDo/View/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  ToDo
+//
+//  Created by Tomoki Yoza on 2025/06/19.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
