c065ac2f4b205caa698922bc15abecad88aa16c7
yukikakuda <yuki3118@go.byuh.edu>
2025-06-19 22:51:28 -1000

    commit yuki

---
+++ ToDo/View/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  ToDo
+//
+//  Created by Yuki Kakuda on 6/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
